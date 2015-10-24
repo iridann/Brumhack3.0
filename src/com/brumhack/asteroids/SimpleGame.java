62d806a5a18c7b68c5f5b5ca7624344d0f099500
Daniel Baryshnikov <daniel_baryshnikov@hotmail.co.uk>
2015-10-24 21:58:11 +0100

    using a image and passing it as a texture into a shape. This allows for collisions to be handled between asteroid and ship, likewise when shooting is implemented.

--- src/com/brumhack/asteroids/SimpleGame.java
+++ src/com/brumhack/asteroids/SimpleGame.java
@@ -1,41 +1,54 @@
 package com.brumhack.asteroids;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.state.StateBasedGame;
 
 
 /**
  * @author Iridann.
  */
+
 public class SimpleGame extends BasicGame {
-    public SimpleGame(String gamename) {
-        super(gamename);
+    private Image ship = null;
+    private Image asteroid1 = null;
+    private Image asteroid2 = null;
+    private Image asteroid3 = null;
+    private Image asteroid4 = null;
+    private Shape circle = null;
+
+    public SimpleGame(String title) {
+        super(title);
     }
 
     @Override
-    public void init(GameContainer gc) throws SlickException {}
-
-    @Override
-    public void update(GameContainer gc, int i) throws SlickException {}
-
-    @Override
-    public void render(GameContainer gc, Graphics g) throws SlickException {
-        g.drawString("Hello World!", 10, 10);
+    public void init(GameContainer container) throws SlickException {
+    ship = new Image("res/photos.007.png");
+        asteroid1 = new Image("res/photos.003.png");
+        asteroid2 = new Image("res/photos.004.png");
+        asteroid3 = new Image("res/photos.005.png");
+        asteroid4 = new Image("res/photos.006.png");
+        circle = new Circle(50,50,300);
     }
 
-    public static void main(String[] args) {
-        try {
-            AppGameContainer appgc;
-            appgc = new AppGameContainer(new SimpleGame("Simple Game"));
-            appgc.setDisplayMode(640, 480, false);
-            appgc.start();
-        } catch (SlickException ex) {
-            Logger.getLogger(SimpleGame.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+    g.texture(circle,ship,true);
     }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+    circle.setCenterX(container.getInput().getMouseX());
+        circle.setCenterY(container.getInput().getMouseY());
+    }
+
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer agc = new AppGameContainer(new SimpleGame("Images"));
+        agc.setDisplayMode(800,640,false);
+        agc.start();
+    }
+
+
 }
