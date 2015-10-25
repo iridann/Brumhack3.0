e070e770ee9c4820f0aaf60dc58037d7727668a5
Janos Potecki <janos.potecki.15@ucl.ac.uk>
2015-10-25 01:19:50 +0100

    getting tired

--- src/com/brumhack/asteroids/Company.java
+++ src/com/brumhack/asteroids/Company.java
@@ -9,24 +9,31 @@ import java.util.Map;
 
 
 public class Company {
+
+
     private class Year {
-        private int[] months = new int[12];
-        private String name;
-        public Year(String name){
+        private int[] months;
+        private int name;
+
+        private Year(int name){
             this.name = name;
+        }
+        public int getName(){
+            return this.name;
         }
 
         public void addValue(int month, int value){
-            if(month >= this.months.length())
+            month--;
+            if(month > 12 || month < 0)
                 throw new RuntimeException("month not in this year");
             this.months[month] = value;
         }
 
         public int getValue(int month){
-            if(month >= this.months.size())
+            month--;
+            if(month > 11 || month < 0)
                 throw new RuntimeException("month not in this year");
-
-            return this.months.get(month);
+            return this.months[month];
         }
 
 
@@ -34,6 +41,7 @@ public class Company {
 
     private Map years = new HashMap<>();
     private String name;
+
     public Company(String name){
         this.name = name;
     }
@@ -42,9 +50,25 @@ public class Company {
         return this.name;
     }
 
-    public addYear(Year year){
-        this.years.put()
+    public void addYear(Year year){
+        this.years.put(year.getName(), year);
     }
+
+    public boolean hasYear(int year){
+        if( this.years.get(year) == null)
+            return false;
+        return true;
+    }
+
+    public void insertMonth(int month, int value, int year){
+        ( (Year)this.years.get(year) ).addValue(month, value);
+    }
+
+
+
+
+
+
 
 
 }
